97ccc7a8802e09f5fc5fcb189ab5ba26bb5827e0
Sabir Ali <motorola50neo@gmail.com>
2025-07-20 08:37:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,12 @@ import os
 
 load_dotenv()
 
-# Dummy user data (initial)
 USERS = {}
 
-# Bot Configuration
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 BOT_USERNAME = os.getenv("BOT_USERNAME")
 
-# Rewards Configuration
 DAILY_REWARD = list(map(int, os.getenv("DAILY_REWARD").split(",")))
 SPIN_REWARDS = list(map(int, os.getenv("SPIN_REWARDS").split(",")))
 GAME_REWARD = int(os.getenv("GAME_REWARD"))
@@ -19,7 +16,6 @@ VIDEO_REWARD = int(os.getenv("VIDEO_REWARD"))
 REFER_REWARD = int(os.getenv("REFER_REWARD"))
 REFER_PERCENT = float(os.getenv("REFER_PERCENT"))
 
-# Withdrawal Settings
 MIN_WITHDRAWAL = int(os.getenv("MIN_WITHDRAWAL"))
 COIN_TO_TAKA = int(os.getenv("COIN_TO_TAKA"))
 WITHDRAW_OPTIONS = list(map(int, os.getenv("WITHDRAW_OPTIONS").split(",")))
