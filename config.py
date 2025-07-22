9610035526eb4d80b48e189c21642f992b749018
Sabir Ali <motorola50neo@gmail.com>
2025-07-22 11:24:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 import os
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
+# Load .env variables
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 BOT_USERNAME = os.getenv("BOT_USERNAME")
+
+# Initialize bot and dispatcher
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
